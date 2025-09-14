231a25a4d08feac8d96fe7960605346e2ff4d045
thinnershapewear-del <thinnershapewear@gmail.com>
2025-09-14 09:30:38 -0500

    Add files via upload

---
+++ stripe (3).js
@@ -0,0 +1,33 @@
+// api/stripe.js
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { priceId } = req.body;
+
+      // Create a Checkout Session
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price: priceId, // price ID from your Stripe dashboard
+            quantity: 1,
+          },
+        ],
+        mode: "subscription",
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/cancel`
+      });
+
+      res.status(200).json({ id: session.id });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
